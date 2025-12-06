9400562ea5c98e863c3ee676bab3829532b00c31
KaeTheDev <kaethedev@gmail.com>
2025-12-06 02:28:17 -0500

    Removed utils folder and file

--- src/utils/text.ts
+++
@@ -1,5 +0,0 @@
-// const characterCount = text.length;
-
-// const wordCount = text.trim() === '' ? 0 : text.trim().split(/\s+/).length;
-
-// const readingTime = Math.round(wordCount / 200); // 200 WPM assumed
